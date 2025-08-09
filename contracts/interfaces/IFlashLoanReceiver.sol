fc3c52b5c2c8e9c324daa97e955a16ea6aab106f
Anik Tahabilder <tahabilderanik@gmail.com>
2025-08-09 10:45:33 -0400

    Added smart contract interfaces for Aave V3 and Chainlink

---
+++ contracts/interfaces/IFlashLoanReceiver.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+interface IFlashLoanSimpleReceiver {
+    function executeOperation(
+        address asset,
+        uint256 amount,
+        uint256 premium,
+        address initiator,
+        bytes calldata params
+    ) external returns (bool);
+
+    function ADDRESSES_PROVIDER() external view returns (address);
+    function POOL() external view returns (address);
+}
